a3ba7aefa09cd5ab844278a28effdfdb875e55ec
Daren Shamoun <darenshamoun@gmail.com>
2021-08-13 18:12:59 -0700

    another update

---
+++ Small Practice Programs/Functions/LongestWord.py
@@ -0,0 +1,16 @@
+# The longest_word function is used to compare 3 words. 
+# It should return the word with the most number of characters 
+# (and the first in the list when they have the same length).
+
+def longest_word(word1, word2, word3):
+	if len(word1) >= len(word2) and len(word1) >= len(word3):
+		word = word1
+	elif len(word2) >= len(word1) and len(word2) >= len(word3):
+		word = word2
+	else:
+		word = word3
+	return(word)
+
+print(longest_word("chair", "couch", "table"))
+print(longest_word("bed", "bath", "beyond"))
+print(longest_word("laptop", "notebook", "desktop"))
---
+++ Small Practice Programs/Loops/SumOfAllDivisors.py
@@ -0,0 +1,11 @@
+# Returns the sum of all the divisors of a number, without including it. 
+# A divisor is a number that divides into another without a remainder.
+
+def sum_divisors(n):
+    return sum([i for i in range(1, n)
+                if n % i == 0])
+
+print(sum_divisors(0))
+print(sum_divisors(3)) # Should sum of 1
+print(sum_divisors(36)) # Should sum of 1+2+3+4+6+9+12+18
+print(sum_divisors(102)) # Should be sum of 2+3+6+17+34+51
